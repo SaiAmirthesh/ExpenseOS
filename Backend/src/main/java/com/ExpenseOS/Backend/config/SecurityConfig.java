01cb107ae51901be87a1315faabcda62358f27ba
SaiAmirthesh <saiamirthesh8419@gmail.com>
2026-05-24 16:33:34 +0530

    feat: Auth service db test connection

--- Backend/src/main/java/com/ExpenseOS/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/ExpenseOS/Backend/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package com.ExpenseOS.Backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +16,28 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/h2-console/**"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                )
+
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
